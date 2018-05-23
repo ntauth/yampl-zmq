9618ae3ada4e8bd8f9b2ab54f069fd9a4f86d643
Ayoub Chouak <a.chouak@protonmail.com>
2018-05-24 01:49:22 +0200

    Edited yampl-zmq.cmake

--- yampl-zmq.cmake
+++ yampl-zmq.cmake
@@ -1,9 +1,7 @@
-#################################
+##
 # YAMPL-ZMQ
-#################################
-if (WITH_ZMQ_PLUGIN STREQUAL ON)
-    message(STATUS "[PLUGIN] Including yampl-zmq")
-	    
+##
+if (WITH_ZMQ_PLUGIN)	    
     add_library(yampl-zmq SHARED
 	    plugins/yampl-zmq/src/ClientSocket.cpp
             plugins/yampl-zmq/src/ServerSocket.cpp
@@ -14,14 +12,10 @@ if (WITH_ZMQ_PLUGIN STREQUAL ON)
 
     target_include_directories(yampl-zmq PRIVATE plugins/yampl-zmq/include)
 
-    set_target_properties(yampl-zmq PROPERTIES SUFFIX ".yam")
     set_target_properties(yampl-zmq
             PROPERTIES
-            ARCHIVE_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/plugins/yampl-zmq"
             LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/plugins/yampl-zmq"
-            RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/plugins/yampl-zmq"
     )
-    add_dependencies(yampl-zmq LibSourcey)
 
     install(TARGETS yampl-zmq
             LIBRARY DESTINATION ${CMAKE_INSTALL_PREFIX}/lib/yampl/plugins/yampl-zmq
